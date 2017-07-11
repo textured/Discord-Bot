989b9ccca6b4a4412d8bf8e2ccf7f0ed6d183426
textured <danielphan123@gmail.com>
2017-07-11 02:48:15 -0700

    added additional commands for dota2

--- bot.py
+++ bot.py
@@ -5,8 +5,20 @@ from discord.ext import commands
 # Initialize bot with '!' prefix
 botTextured = commands.Bot(command_prefix='!')
 
+dotaAPI = dota2api.Initialise("STEAM_WEBAPI_KEY")
+
+
+"""
+Associate dota2 ID's of players with key of discord ID
+e.g 'sample#0123': 11111111
+"""
+usersDotaID = {
+"sample#0123": 11111111
+}
+
+
 # games list
-games = ['dota 2', 'Battlefield 1', 'minecraft']
+games = ["insert example games here"]
 
 @botTextured.event
 async def on_ready():
@@ -42,6 +54,37 @@ async def commands():
     """ say list of commands available to the user """
     await botTextured.say("available commands")
     await botTextured.say("!flip !game !feels")
+
+# Dota 2 related commands
+
+@botRaymond.command(pass_context=True)
+async def recentgame(ctx):
+    """
+    Retrives link to latest dota game dependant on caller via dotabuff
+
+    """
+    url = 'https://www.dotabuff.com/matches/'
+    user_id = str(ctx.message.author)
+    dota_id = usersDotaID[user_id]
+    hist = dotaAPI.get_match_history(account_id=dota_id)
+    matchid = str(hist['matches'][0]['match_id'])
+    await botRaymond.say(content="{}".format(url + matchid))
+
+@botRaymond.command(pass_context=True)
+async def dotabuff(ctx, arg = None):
+    """
+    Retrive dotabuff account (Optional argument to perform search)
+    e.g !dotabuff Dendi performs a search for users named Dendi
+
+    """
+    if arg == None:
+        url = 'https://www.dotabuff.com/players/'
+        user_id = str(ctx.message.author)
+        player_id = str(usersDotaID[user_id])
+        await botRaymond.say(content="{}".format(url + player_id))
+    elif arg != None:
+        url = 'https://www.dotabuff.com/search?utf8=%E2%9C%93&q={}&commit=Search'.format(arg)
+        await botRaymond.say(content="{}".format(url))
 
 # run bot with token acquired from Discord
 botTextured.run("TOKEN_HERE")
